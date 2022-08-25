e38e4e5bdc8e68ebdefdb2e9706afb8f131703a5
Ayaan Raza <108168382+ayaanraza9@users.noreply.github.com>
2022-08-25 15:13:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,48 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect} from "react";
 import '../styles/App.css';
 
+
+
 const App = () => {
-  // write your code here 
+
+  const [timeLeft , setTimeleft] = useState(0);
+
+  const handleKey = (event) =>{
+    if(event.key == "Enter"){
+      var number = document.getElementById("timeCount").value;
+      console.log(number);
+      if(isNaN(number)){
+        console.log("not a number");
+        document.getElementById("timeCount").value='0';
+        return;
+      }
+      setTimeleft(parseInt(number));
+    }
+  }
+
+  var intervalId;
+
+  useEffect(()=>{
+
+      if(timeLeft > 0){
+        intervalId = setInterval(()=>{
+          setTimeleft((timeLeft)=>timeLeft-1);
+        },1000);
+      }
+      return () => {
+        clearInterval(intervalId); //clearing the last
+      };
+  },[handleKey]);
 
   return (
+    
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={handleKey} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timeLeft}</div>
     </div>
   )
 }
